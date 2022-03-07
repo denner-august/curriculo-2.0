ea0b2caae2b2dbf11bdd64d0c0ee4082b0a6d7cc
Denner augusto De Paula Bernardes <41222616+denner-august@users.noreply.github.com>
2022-03-07 16:33:56 -0300

    Update index.tsx

--- src/components/social-media/index.tsx
+++ src/components/social-media/index.tsx
@@ -1,3 +1,75 @@
+import Image from "next/image";
+import { Container } from "./styles";
+
+import Linkedin from "../../../public/image/social-icons/linkedin.jpeg";
+
+import Github from "../../../public/image/social-icons/github.png";
+import whatsapp from "../../../public/image/social-icons/whatsapp.png";
+import Email from "../../../public/image/social-icons/email.png";
+
 export default function Social_Media() {
-  return <div className="social-media"></div>;
+  return (
+    <Container className="social-media">
+      <ul>
+        <li>
+          <Image
+            width={50}
+            height={50}
+            objectFit="contain"
+            className="social-media"
+            src={Linkedin}
+            alt="Linkedin"
+          />
+          <a
+            href="https://www.linkedin.com/in/denner-bernardes"
+            target={"black"}
+          >
+            Linkedin
+          </a>
+        </li>
+        <li>
+          <Image
+            width={50}
+            height={50}
+            objectFit="contain"
+            className="social-media"
+            src={Github}
+            alt="Github"
+          />
+          <a href="https://github.com/denner-august" target={"black"}>
+            Github
+          </a>
+        </li>
+        <li>
+          <Image
+            width={50}
+            height={50}
+            objectFit="contain"
+            className="social-media"
+            src={whatsapp}
+            alt="whatsapp"
+          />
+          <a
+            href="https://api.whatsapp.com/send?phone=5511978057417&text="
+            target={"black"}
+          >
+            Whatsapp
+          </a>
+        </li>
+        <li>
+          <Image
+            width={50}
+            height={50}
+            objectFit="contain"
+            className="social-media"
+            src={Email}
+            alt="whatsapp"
+          />
+          <a href="mailto:denner.augusto90@gmail.com">
+            <p>denner.augusto90@gmail.com</p>
+          </a>
+        </li>
+      </ul>
+    </Container>
+  );
 }
