20f98a75703fdc636f6c1bff96c182e1d007d53b
Denner augusto De Paula Bernardes <41222616+denner-august@users.noreply.github.com>
2022-03-07 16:34:01 -0300

    Update index.tsx

--- src/components/profile/index.tsx
+++ src/components/profile/index.tsx
@@ -1,3 +1,14 @@
+import { Container } from "./styles";
+import Image from "next/image";
+import ImageProfile from "../../../public/image/perfil.jpeg";
 export default function Profile() {
-  return <div className="profile"></div>;
+  return (
+    <Container className="profile">
+      <div className="imagem">
+        <Image src={ImageProfile} height={300} width={200} objectFit="fill" />
+      </div>
+      <p>Denner Augusto</p>
+      <p>Developer front-end</p>
+    </Container>
+  );
 }
