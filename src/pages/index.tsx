b249ad13e0b60716ed589acfaece3bb2fd960a48
Denner augusto De Paula Bernardes <41222616+denner-august@users.noreply.github.com>
2022-03-07 11:47:49 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,28 @@
+import Experience from "../components/experience";
+import Skills from "../components/skills";
+import Social from "../components/social";
+import Social_Media from "../components/social-media";
+import { ContainerPrincipal } from "../styles/styles";
+import Education from "../components/education/index";
+import Profile from "../components/profile";
+import Project01 from "../components/project-01/index";
+import Project02 from "../components/project-02";
+import Post from "../components/recent-posts";
+import Projects from "../components/projects/index";
+
 export default function Principal() {
-  return <h1>Curriculo</h1>;
+  return (
+    <ContainerPrincipal className="container">
+      <Profile />
+      <Social />
+      <Social_Media />
+      <Skills />
+      <Experience />
+      <Education />
+      <Projects />
+      <Project01 />
+      <Project02 />
+      <Post />
+    </ContainerPrincipal>
+  );
 }
