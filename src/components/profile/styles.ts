f58c152b61bccd5f779d5abd07a8fbbd0152d36e
Denner augusto De Paula Bernardes <41222616+denner-august@users.noreply.github.com>
2022-03-07 16:33:59 -0300

    Create styles.ts

---
+++ src/components/profile/styles.ts
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display:flex ;
+    flex-direction: column ;
+    justify-content: space-between ;
+    align-items:center;
+   
+    .imagem{
+        background-color:var( --bg-cards);
+        border-radius: 10%;
+    }
+
+    p{  
+        box-sizing: border-box ;
+        font-size:1.5rem;
+        text-align:  center;
+        background-color:var( --bg-cards);
+
+    }
+
+
+
+    
+
+`
