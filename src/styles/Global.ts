32cb6aa3c17c8608ce026b071d4723096cb77661
Denner augusto De Paula Bernardes <41222616+denner-august@users.noreply.github.com>
2022-03-07 11:47:44 -0300

    Update Global.ts

--- src/styles/Global.ts
+++ src/styles/Global.ts
@@ -1,23 +1,78 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
- :root{
-        --background:#ECF0F1;
-        --red: #e52e4d;
-        --blue:#5429cc;
-        --green:#33cc95;
-        --blue-ligth:#6933ff;
-        --text-title:#363f5f;
-        --text-body:#969cb3;
-        --shape:#ffff;
-
-    }
+:root {
+  --body-bg-color: #22212C;
+  --text-color: #837E9F;
+  --bg-cards: #302F3D;
+  --bg-techs: #CB92B1;
+  --bord-card:10px
+}
 
 *{
+    height: 100% ;
     margin: 0;
-    padding: 0;
+    padding: 0.5rem;
     box-sizing: border-box;
+    background-color:  var(--body-bg-color);
 }
+
+h1,h2,p{
+    color:var(--text-color);
+}
+
+.container{
+  display: grid;
+  grid-template-columns: 1fr 2fr 2fr;
+  grid-template-rows: 1fr 1fr 1fr 1fr 1fr;
+  grid-auto-columns: 1fr;
+  gap: 10px 10px;
+  grid-auto-flow: row;
+  height: 150vh ;
+}
+
+.social { 
+grid-area: 2 / 1 / 3 / 2; 
+background-color:var( --bg-cards);
+border-radius:var(--bord-card)   }
+
+.social-media {   
+grid-area: 2 / 1 / 3 / 2;
+background-color:var( --bg-cards);
+border-radius:var(--bord-card)  }
+
+.profile { grid-area: 1 / 1 / 2 / 2;
+background-color:var( --bg-cards);
+border-radius:var(--bord-card)  
+}
+
+.skills { grid-area: 3 / 1 / 4 / 2;
+background-color:var( --bg-cards);
+border-radius:var(--bord-card) }
+
+.experience { grid-area: 4 / 1 / 5 / 2;
+background-color:var( --bg-cards);
+border-radius:var(--bord-card) }
+
+.education { grid-area: 5 / 1 / 6 / 2;
+background-color:var( --bg-cards);
+border-radius:var(--bord-card) }
+
+.projects { grid-area: 1 / 2 / 2 / 4; 
+background-color:var( --bg-cards); 
+border-radius:var(--bord-card) }
+
+.project-01 { grid-area: 2 / 2 / 3 / 3; 
+background-color:var( --bg-cards);
+border-radius:var(--bord-card) }
+
+.project-02 { grid-area: 2 / 3 / 3 / 4;
+background-color:var( --bg-cards);
+border-radius:var(--bord-card) }
+
+.recent-posts { grid-area: 3 / 2 / 4 / 4;
+background-color:var( --bg-cards);
+border-radius:var(--bord-card) }
 
 
 `
