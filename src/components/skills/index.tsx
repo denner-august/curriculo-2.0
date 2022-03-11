64566f597144b69ce8eb0552e96fa97d66c6cf70
Denner Augusto De Paula Bernardes <88399382+syg-Denner@users.noreply.github.com>
2022-03-11 12:39:09 -0300

    Update index.tsx

--- src/components/skills/index.tsx
+++ src/components/skills/index.tsx
@@ -1,3 +1,28 @@
+import { Container } from "./styles";
+
 export default function Skills() {
-  return <div className="skills"></div>;
+  const tecnologias = [
+    "Javascript",
+    "Reactjs",
+    "Git",
+    "Github",
+    "Html",
+    "Css",
+    "Scss",
+    "Typescript",
+    "Styled components",
+    "Postgres",
+    "Modelagem de dados",
+  ];
+
+  const renderTecnologia = () => {
+    return tecnologias.map((item, index) => <li key={index}>{item}</li>);
+  };
+
+  return (
+    <Container className="skills">
+      <p>Tecnologias</p>
+      <ul>{renderTecnologia()}</ul>
+    </Container>
+  );
 }
