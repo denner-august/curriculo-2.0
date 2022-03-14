a0af4b90a40e23b6c1c7edb9b781980ab709a3a0
Denner Augusto De Paula Bernardes <41222616+denner-august@users.noreply.github.com>
2022-03-14 20:44:17 -0300

    Update index.tsx

--- src/components/education/index.tsx
+++ src/components/education/index.tsx
@@ -1,3 +1,18 @@
+import { Container } from "./styles";
+
 export default function Education() {
-  return <div className="education"></div>;
+  return (
+    <Container className="education">
+      <p>Educação</p>
+      <ul>
+        <li>
+          Graduação: Analise e desenvolvimento de sistemas <br /> Atualmente:
+          Quarto semestre <br /> Previsão de termino: final de 2022
+        </li>
+        <li>
+          Rocketseat: Ignite 2021 <br /> Previsão de termino: final de 2022
+        </li>
+      </ul>
+    </Container>
+  );
 }
