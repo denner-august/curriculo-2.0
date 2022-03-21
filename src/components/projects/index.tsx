923f2174fe4060c8a39d4bd681ee55f95768d7d3
Denner augusto De Paula Bernardes <41222616+denner-august@users.noreply.github.com>
2022-03-21 18:47:01 -0300

    implementando api

--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -1,6 +1,23 @@
+import { useEffect } from "react";
 import { Container } from "./styles";
 import Link from "next/link";
+import axios from "axios";
 export default function Projects() {
+  useEffect(() => {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_VERCEL_KEY}
+      `,
+      },
+    };
+    axios
+      .get(
+        "https://api.vercel.com/v6/projects/prj_aBNIXDGrbtJBOqO3qeShnRB8C6XF",
+        config
+      )
+      .then((response) => console.log(response.data.alias[0].domain));
+  });
+
   return (
     <Container className="projects">
       <h2>Meus projetos</h2>
