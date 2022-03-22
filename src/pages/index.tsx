ec8c32e99495d8c9633266341292e0a8c689b1c5
Denner augusto De Paula Bernardes <41222616+denner-august@users.noreply.github.com>
2022-03-21 22:10:30 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,8 +9,9 @@ import Project01 from "../components/project-01/index";
 import Project02 from "../components/project-02";
 import Post from "../components/recent-posts";
 import Projects from "../components/projects/index";
+import axios from "axios";
 
-export default function Principal() {
+export default function Principal({ dados }: any) {
   return (
     <ContainerPrincipal className="container">
       <Profile />
@@ -19,10 +20,28 @@ export default function Principal() {
       <Skills />
       <Experience />
       <Education />
-      <Projects />
-      <Project01 />
+      <Projects dados={dados} />
+      <Project01 dados={dados} />
       <Project02 />
       <Post />
     </ContainerPrincipal>
   );
+}
+
+export async function getStaticProps() {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_VERCEL_KEY}`,
+    },
+  };
+
+  const request = axios
+    .get("https://api.vercel.com/v6/projects", config)
+    .then((response) => response);
+
+  return {
+    props: {
+      dados: await request,
+    },
+  };
 }
