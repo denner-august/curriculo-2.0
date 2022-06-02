5185d152419cb12285102816ac24e412f5c97c27
Denner augusto De Paula Bernardes <41222616+denner-august@users.noreply.github.com>
2022-06-02 18:40:25 -0300

    ajustes de layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,20 @@
-import { AppProps } from "next/app";
 import React from "react";
-import {GlobalStyle} from "../styles/Global";
-function MyApp({ Component, pageProps }:any) {
+import { GlobalStyle } from "../styles/Global";
+import Head from "next/head";
+import favicon from "../../public/image/social-icons/linkedin.jpeg";
+
+function MyApp({ Component, pageProps }: any) {
   return (
     <>
-      <Component {...pageProps}/>
+      <Head>
+        <title>Portfólio</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link
+          rel="shortcut icon"
+          href="https://cdn-icons.flaticon.com/png/512/924/premium/924915.png?token=exp=1654206871~hmac=f0efbb710376adcaa41e9739db8680e1"
+        />
+      </Head>
+      <Component {...pageProps} />
       <GlobalStyle />
     </>
   );
