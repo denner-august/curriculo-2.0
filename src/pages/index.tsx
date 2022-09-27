8e1010b4e03846dee7699b8618817ede0d941203
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2022-09-27 16:19:02 -0300

    configurando prismic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,24 @@
 import Experience from "../components/experience";
 import Skills from "../components/skills";
 import Social_Media from "../components/social-media";
-import { ContainerPrincipal } from "../styles/styles";
 import Education from "../components/education/index";
 import Profile from "../components/profile";
 import Projects from "../components/projects/index";
 import axios from "axios";
 import { GetStaticProps } from "next";
+import { ContainerPrincipal } from "../styles/styles";
+import { filtraUrlName } from "../../services/projetos";
+import { getPrismicClient } from "../../services/prismicClient";
 
-export default function Principal({ dados }: { dados: { item: string } }) {
+type PrincipalProps = {
+  dados: { item: string };
+  post: {
+    titulo: string;
+    conteudo: { paragrafo1: []; paragra2: []; paragrafo3: [] };
+  };
+};
+
+export default function Principal({ dados, post }: PrincipalProps) {
   return (
     <ContainerPrincipal className="container">
       <Profile />
@@ -33,57 +43,16 @@ export const getStaticProps: GetStaticProps = async () => {
     .then((response) => response.data)
     .then((response) => response.projects);
 
-  const projects = [
-    "praticando-meu-front-end-react-js",
-    "redux-learning",
-    "nlw-return",
-    "front-end-challenge-coodesh",
-    "fills-calcuer",
-    "capitulo-2-ignite",
-    "room-homepage",
-    "formulario-de-inscricao",
-    "testimonial-from-users",
-    "hoteliour",
-    "fale-muito-mais-agora",
-    "ignite-interface-com-chakra-ui",
-    "ignite-fundamentos-next",
-  ];
+  const prismic = getPrismicClient();
 
-  interface ProjectProps {
-    targets: { production: { name: string; url: string } };
-  }
+  const prismicRequest = await prismic.getAllByType("teste");
 
-  function filtraUrlName() {
-    const findProjects = projects.map((item) => {
-      return request.find(
-        (data: { targets: { production: { name: string } } }) =>
-          data.targets.production.name === item
-      );
-    });
-
-    const filterProjects = findProjects.map((item: ProjectProps) => {
-      return {
-        name: item.targets.production.name,
-
-        url:
-          item.targets.production.name === "capitulo-2-ignite"
-            ? "capitulo-2-ignite.vercel.app/"
-            : item.targets.production.name === "fills-calcuer"
-            ? "fills-calculator.vercel.app"
-            : item.targets.production.name === "ignite-interface-com-chakra-ui"
-            ? "worldtrip5-chakra.vercel.app"
-            : item.targets.production.name === "ignite-fundamentos-next"
-            ? "ignite-fundamentos-next.vercel.app"
-            : item.targets.production.url,
-      };
-    });
-
-    return filterProjects;
-  }
+  const DataRequest = prismicRequest.map((post) => post.data);
 
   return {
     props: {
-      dados: filtraUrlName(),
+      dados: filtraUrlName(request),
+      posts: DataRequest,
     },
   };
 };
