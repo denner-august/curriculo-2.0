87d9bfb9a953d9439bd3d27295032c423c99b5bd
denner augusto <denner.augusto90@gmail.com>
2023-03-08 19:09:59 -0300

    Update index.tsx

--- src/components/education/index.tsx
+++ src/components/education/index.tsx
@@ -6,25 +6,16 @@ export default function Education() {
       <p>Cursos e especializações</p>
       <ul>
         <li>
+          Graduação: Analise e desenvolvimento de sistemas <br></br> termino: dezembro 2022
+        </li>
+        <li>
+          Rocketseat: Ignite 2021 <br /> termino: outubro 2022
+        </li>
+        <li>
           Desenvolvimento Ágil de Software <br /> termino: 21 de maio de 2022
         </li>
         <li>
           Git e GitHub: Formação Básica <br /> termino: 27 de maio de 2022
-        </li>
-        <li>
-          Descubra o CSS <br /> termino:17 de maio de 2022
-        </li>
-        <li>
-          Competências Profissionais, Emocionais e Tecnológicas para Tempos de
-          Mudança <br /> termino: 28 de Agosto de 2020
-        </li>
-
-        <li>
-          Graduação: Analise e desenvolvimento de sistemas <br /> Atualmente:
-          Quarto semestre <br /> Previsão de termino: final de 2022
-        </li>
-        <li>
-          Rocketseat: Ignite 2021 <br /> Previsão de termino: final de 2022
         </li>
       </ul>
     </Container>
