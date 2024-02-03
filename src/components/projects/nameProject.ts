e3fc7801344da1f4db220f684fd5881e150fb8fb
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2024-02-03 14:34:50 -0300

    Create nameProject.ts

---
+++ src/components/projects/nameProject.ts
@@ -0,0 +1,23 @@
+export function changeNames(name: string) {
+  if (name === "capitulo-2-ignite") {
+    return "Dt-money";
+  }
+
+  if (name === "ignite-fundamentos-next") {
+    return "Ig.news";
+  }
+
+  if (name === "front-end-code-challenge-smartsite") {
+    return "smartsite";
+  }
+
+  if (name === "front-end-challenge-coodesh") {
+    return "coodesh-challenge";
+  }
+
+  if (name === "praticando-meu-front-end-react-js") {
+    return "project front-end";
+  }
+
+  return name;
+}
