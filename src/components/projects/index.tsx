d938b3ce0cfa5c69b57cc231ef1b06f859556c81
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2024-02-03 14:34:44 -0300

    Update index.tsx

--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -1,56 +1,17 @@
-import { useState, useEffect } from "react";
+import { Suspense } from "react";
+import { GetProjects } from "./getProjects";
 import { Container } from "./styles";
-import { getProject } from "@/../services/requestProject";
 
-interface ProjectsProps {
-  name: string;
-  url: string;
-}
-
-export default function Projects() {
-  const [dados, setProjects] = useState([] as ProjectsProps[]);
-
-  useEffect(() => {
-    async function GetProjects() {
-      const dados = await getProject();
-      setProjects(dados);
-    }
-    GetProjects();
-  }, []);
-
-  function changeNames(name: string) {
-    if (name === "capitulo-2-ignite") {
-      return "Dt-money";
-    }
-
-    if (name === "ignite-fundamentos-next") {
-      return "Ig.news";
-    }
-
-    return name;
-  }
-
-  function RenderList() {
-    return dados.map((item: ProjectsProps, index: number) => {
-      return (
-        <li key={index}>
-          <a
-            href={`https://${item.url}`}
-            target={"_blank"}
-            rel="noopener noreferrer"
-          >
-            {changeNames(item.name)}
-          </a>
-        </li>
-      );
-    });
-  }
-
+export default async function Projects() {
   return (
     <Container className="projects">
       <h2>Projetos Desenvolvidos</h2>
 
-      <ul>{RenderList()}</ul>
+      <ul>
+        <Suspense fallback={<h1>carrregando</h1>}>
+          <GetProjects />
+        </Suspense>
+      </ul>
     </Container>
   );
 }
