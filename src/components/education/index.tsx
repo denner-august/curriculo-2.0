2d77bf4aba9ad8bbf7c1f8ee9a523df601f56f93
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2024-04-04 19:57:41 -0300

    Update index.tsx

--- src/components/education/index.tsx
+++ src/components/education/index.tsx
@@ -1,26 +1,42 @@
+import { RenderCursos } from "../../../tools/renderCursos";
 import { Container } from "./styles";
 
 export default function Education() {
   return (
     <Container className="education">
       <p>Cursos e especializações</p>
+
       <ul>
-        <li>
-          FreeCodeCamp: Responsive Web Design <br></br> termino: Março 2024
-        </li>
-        <li>
-          Graduação: Analise e desenvolvimento de sistemas <br></br> termino:
-          dezembro 2022
-        </li>
-        <li>
-          Rocketseat: Ignite 2021 <br /> termino: outubro 2022
-        </li>
-        <li>
-          Desenvolvimento Ágil de Software <br /> termino: 21 de maio de 2022
-        </li>
-        <li>
-          Git e GitHub: Formação Básica <br /> termino: 27 de maio de 2022
-        </li>
+        <RenderCursos
+          nome="CC50: Introdução à Ciência da Computação - O Curso de Harvard, no Brasil"
+          data="Março 2024"
+        />
+
+        <RenderCursos
+          nome=" FreeCodeCamp: Responsive Web Design"
+          data=" Março 2024"
+        />
+        <RenderCursos
+          nome="Graduação: Analise e desenvolvimento de sistemas"
+          data="
+          dezembro 2022"
+        />
+        <RenderCursos
+          nome="Rocketseat: Ignite 2021 "
+          data="
+          outubro 2022"
+        />
+
+        <RenderCursos
+          nome="Desenvolvimento Ágil de Software "
+          data="
+           maio de 2022"
+        />
+        <RenderCursos
+          nome="  Git e GitHub: Formação Básica "
+          data="
+          maio de 2022"
+        />
       </ul>
     </Container>
   );
